f19e878460fea2c517d5d00ecb07ccd4994115f7
unknown <erika.lyde@gmail.com>
2021-07-07 11:49:43 -0300

    cambios completos

--- src/main/java/ar/com/learsoft/javaws/MyController.java
+++ src/main/java/ar/com/learsoft/javaws/MyController.java
@@ -12,47 +12,10 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import ar.com.learsoft.javaws.model.Client;
-import ar.com.learsoft.javaws.repository.ClientRepository;
 
 @RestController
 public class MyController {
     
-	@Autowired 
-	private ClientRepository clientRepository;
 	
-	
-	//CRUD CLIENT
-	
-	//CREATE
-	@PostMapping("/client")
-	public Client createClient(@RequestBody Client client) {
-		return clientRepository.save(client);
-	}
-	
-	//READ
-	@GetMapping("/client/{id}")
-	public Client readClient(@PathVariable Integer id) {
-		Client client = clientRepository.getOne(id);
-		return client;
-	}
-	
-	//UPDATE
-	@PatchMapping("/client")
-	public Client updateClient(@RequestBody Client clientUpdate) {
-		Client client = clientRepository.getOne(clientUpdate.getId());
-		client.setApellido(clientUpdate.getApellido());
-		client.setNombre(clientUpdate.getNombre());
-		return clientRepository.save(client);
-	}
-	
-	//DELETE	
-	@DeleteMapping("/client/{id}")
-	public String deleteClient(@PathVariable Integer id) {
-		clientRepository.deleteById(id);
-		return "El cliente " + id + " fue borrado";
-	}
-	
-
   
 }
--- src/main/java/ar/com/learsoft/javaws/StartApplication.java
+++ src/main/java/ar/com/learsoft/javaws/StartApplication.java
@@ -3,7 +3,7 @@ package ar.com.learsoft.javaws;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import ar.com.learsoft.javaws.model.Client;
+
 
 @SpringBootApplication
 public class StartApplication {
