0ac51bdcd028ee0de03808376ce6f2551239fe32
MarcelaGonzalezz <marcefg1999@gmail.com>
2021-07-07 14:44:36 -0300

    readInvoice

--- src/main/java/ar/com/learsoft/javaws/MyController.java
+++ src/main/java/ar/com/learsoft/javaws/MyController.java
@@ -1,6 +1,8 @@
 package ar.com.learsoft.javaws;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import ar.com.learsoft.javaws.model.Factura;
@@ -16,4 +18,9 @@ public class MyController {
 		return facturaRepository.save(factura);
 	}
 
+	@GetMapping("factura/{id}")
+	public Factura readInvoice(@PathVariable Integer id) {
+		Factura invoice = facturaRepository.getOne(id);
+		return invoice;
+	}
 }
