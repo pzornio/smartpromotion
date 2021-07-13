43c885c7e7974c1195f441e1cee0135e73b7b22d
Pablo <pablogilman@hotmail.com>
2021-07-13 09:14:57 -0300

    Se modifico invoice para crear la relacion entre esta y cliente, y se arreglo el get de invoice

--- src/main/java/ar/com/learsoft/smartpromotion/repository/PromotionRepository.java
+++ src/main/java/ar/com/learsoft/smartpromotion/repository/PromotionRepository.java
@@ -11,10 +11,5 @@ import ar.com.learsoft.smartpromotion.model.Promotion;
 
 @Repository
 public interface PromotionRepository extends JpaRepository<Promotion,Integer>{
-	@Query(value="SELECT i.ID,i.AMOUNT,i.CHANNEL,i.DATE,i.PAYMENT_METHOD,i.CLIENT_ID "
-		    + "FROM INVOICE i JOIN INVOICE_PRODUCTS ip "
-		    + "WHERE i.ID = ip.INVOICE_ID "
-		    + "AND i.CLIENT_ID = :clientId "
-		    + "AND ip.PRODUCTS_ID = :productId", nativeQuery = true)
-		    public List<Invoice> findClientProductInvoices(Integer clientId,Integer productId);
+	
 }
