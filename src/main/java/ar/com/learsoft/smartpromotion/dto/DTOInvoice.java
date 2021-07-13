b8ced90a2b55fe301f50975e88cea8cd73c89f3c
Horacio <horacio.perez@gmail.com>
2021-07-13 17:10:15 -0300

    DTOs invoice

--- src/main/java/ar/com/learsoft/smartpromotion/dto/DTOInvoice.java
+++ src/main/java/ar/com/learsoft/smartpromotion/dto/DTOInvoice.java
@@ -1,6 +1,11 @@
 package ar.com.learsoft.smartpromotion.dto;
 
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 import ar.com.learsoft.smartpromotion.model.Invoice;
+import ar.com.learsoft.smartpromotion.model.Product;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -9,6 +14,29 @@ import lombok.ToString;
 @Getter
 @ToString 
 public class DTOInvoice extends DTOGeneric {
-	private Invoice invoice;
-
+	private Integer invoiceId;
+	private Integer clientId;
+	private List<Integer> productIds;
+	private Date date;
+	private Double amount;
+	private String descuento;
+	private Integer cantidad;
+	private String paymentMethod;
+	private String channel;
+	
+	
+	public void setInvoice(Invoice invoice) {
+		this.invoiceId = invoice.getId();
+		this.clientId = invoice.getClient().getId();
+		this.productIds = new ArrayList<>();
+		for (Product product : invoice.getProducts()) {
+			productIds.add(product.getId());
+		}
+		this.date = invoice.getDate();
+		this.amount = invoice.getAmount();
+		this.descuento = invoice.getDescuento();
+		this.cantidad = invoice.getCantidad();
+		this.paymentMethod = invoice.getPaymentMethod();
+		this.channel = invoice.getChannel();
+	}
 }
